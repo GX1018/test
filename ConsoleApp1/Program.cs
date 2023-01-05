a5e94a602e5a89c2a830ddd52148afa435cc5fad
GX1018 <wjdghlxo@gmail.com>
2023-01-05 10:35:49 +0900

    test1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+
+        public class Ruler
+        {
+            private const float ONE_INCH = 2.54f;
+
+            public int Centimeter { get; set; } = 0;
+
+            public float Inch
+            {
+                get { return Centimeter * ONE_INCH; }
+                private set { Centimeter = (int)(value / ONE_INCH); }
+            }
+            public Ruler(int cmvalue) { Centimeter=cmvalue; }
+
+            public void Run()
+            {
+                Console.WriteLine($"{Centimeter}cm 는 {Inch}inch 입니다.");
+            }
+        }
+    }
+}
